28da73d6a974942d4d035eec5e341a5d9d821634
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2025-09-22 18:29:28 +0200

    Refactor YaKRemover to use tail recursion for improved performance and clarity. Update removeYaks function signature to include an accumulator.

--- src/main/scala/YaKRemover.scala
+++ src/main/scala/YaKRemover.scala
@@ -1,9 +1,11 @@
 @main
 def yak =
   println(s"Hello, YaKRemover! ${removeYaks("yykkyokak")}")
+import scala.annotation.tailrec
 
-def removeYaks(s: String): String =
+@tailrec
+def removeYaks(s: String, acc: String = ""): String =
   s.toList match
-    case 'y' :: _ :: 'k' :: tail => removeYaks(tail.mkString)
-    case head :: tail            => head + removeYaks(tail.mkString)
-    case Nil                     => ""
+    case 'y' :: _ :: 'k' :: tail => removeYaks(tail.mkString, acc)
+    case head :: tail            => removeYaks(tail.mkString, acc + head)
+    case Nil                     => acc
