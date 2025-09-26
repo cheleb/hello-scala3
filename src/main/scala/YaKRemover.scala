88580ba21a3a63ea19b0d62c4f869c0c1e8af02c
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2025-09-26 15:22:56 +0200

    yakRemover

--- src/main/scala/YaKRemover.scala
+++ src/main/scala/YaKRemover.scala
@@ -1,11 +1,11 @@
 @main
 def yak =
-  println(s"Hello, YaKRemover! ${removeYaks("yykkyokak")}")
+  println(s"Hello, YaKRemover! ${removeYaks("yykkyokak".toList)}")
 import scala.annotation.tailrec
 
 @tailrec
-def removeYaks(s: String, acc: String = ""): String =
-  s.toList match
-    case 'y' :: _ :: 'k' :: tail => removeYaks(tail.mkString, acc)
-    case head :: tail            => removeYaks(tail.mkString, acc + head)
+def removeYaks(s: List[Char], acc: String = ""): String =
+  s match
+    case 'y' :: _ :: 'k' :: tail => removeYaks(tail, acc)
+    case head :: tail            => removeYaks(tail, acc + head)
     case Nil                     => acc
